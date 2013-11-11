ed4565d41f3082b897c725e48c54f7b281f95720
Ryan Aprill <ryan.aprill@gmail.com>
2013-11-11 16:32:54 -0600

    Installed Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Pinterest::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #Required for Devise
+  config.action_mailer.default_url_options = { :host => 'http://ryan-pinterest.herokuapp.com/' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Pinterest::Application.routes.draw do
+  devise_for :users
   root "pages#home"
   get "about" => "pages#about"
   
