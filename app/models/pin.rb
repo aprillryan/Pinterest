a974d36a32f1b018742daee64ff07e0ed6b2d059
Ryan Aprill <ryan.aprill@gmail.com>
2013-11-12 16:23:32 -0600

    add image upload with paperclip

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,4 @@
 class Pin < ActiveRecord::Base
-	belongs_to :user
+     belongs_to :user
+     has_attached_file :image, :styles => { :small => "500x500>", :medium => "300x300>", :thumb => "100x100>" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131112203633) do
+ActiveRecord::Schema.define(version: 20131112213809) do
 
   create_table "installs", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -38,6 +38,10 @@ ActiveRecord::Schema.define(version: 20131112203633) do
     t.string   "user_id"
     t.string   "integer"
     t.string   "index"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: true do |t|
