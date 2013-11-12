7f8f8d0c410ff441d47cd7d6202fcbeaf0417f78
Ryan Aprill <ryan.aprill@gmail.com>
2013-11-12 16:59:01 -0600

    Add Amazon S3 for Paperclip to Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,14 @@ Pinterest::Application.configure do
 
   #Required for Devise
   config.action_mailer.default_url_options = { :host => 'http://ryan-pinterest.herokuapp.com/' }
+
+  #Used to save images on Heroku with Paperclip
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
