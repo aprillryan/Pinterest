0ae56ca185130215d3237307332465463bb2deb0
Ryan Aprill <ryan.aprill@gmail.com>
2013-11-12 13:28:08 -0600

    added pin scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Pinterest::Application.routes.draw do
+  resources :pins
+
   devise_for :users
   root "pages#home"
   get "about" => "pages#about"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131111223134) do
+ActiveRecord::Schema.define(version: 20131112183203) do
 
   create_table "installs", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 20131111223134) do
 
   add_index "installs", ["email"], name: "index_installs_on_email", unique: true
   add_index "installs", ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true
+
+  create_table "pins", force: true do |t|
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
