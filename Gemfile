813293a2ccf558e50d8b1e1ba9663e3463aeea2f
Ryan Aprill <ryan.aprill@gmail.com>
2013-11-12 18:47:43 -0600

    Replaced emails with name

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem 'paperclip', '~>3.0'
 gem 'aws-sdk', '~>1.20.0'
 gem 'jquery-turbolinks'
 gem 'masonry-rails', '~> 0.2.0'
+gem 'will_paginate', '~> 3.0'
+gem 'will_paginate-bootstrap'
 
 group :development, :test do
 	gem 'sqlite3'
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,7 @@
 class Pin < ActiveRecord::Base
      belongs_to :user
      has_attached_file :image, :styles => { :small => "500x500>", :medium => "300x300>", :thumb => "100x100>" }
+
+  validates :image, presence: true
+  validates :description, presence: true
 end
